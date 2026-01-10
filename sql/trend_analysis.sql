6e3c726e791875efa4a9f37b76225d1321470a19
Chandhana sanoj <chandhanasanoj03@gmail.com>
2026-01-10 22:38:33 +0530

    Update trend_analysis.sql

--- sql/trend_analysis.sql
+++ sql/trend_analysis.sql
@@ -1,1 +1,14 @@
+-- Trend: Total orders by day of week
+SELECT 
+    DATENAME(DW, order_date) AS order_day,
+    COUNT(DISTINCT order_id) AS total_orders 
+FROM pizza_sales
+GROUP BY DATENAME(DW, order_date);
 
+-- Trend: Total orders by hour of day
+SELECT 
+    DATEPART(HOUR, order_time) AS order_hours,
+    COUNT(DISTINCT order_id) AS total_orders
+FROM pizza_sales
+GROUP BY DATEPART(HOUR, order_time)
+ORDER BY DATEPART(HOUR, order_time);
